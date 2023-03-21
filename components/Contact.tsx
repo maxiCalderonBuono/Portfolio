7c640a8cd850df5a0495f723cc9d6064a407e224
Maxi <calderonmaxi@outlook.com>
2023-03-21 20:23:26 +0100

    test13

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,13 +4,16 @@ import { ChatBot } from "./ChatBot";
 
 const Contact = () => {
   return (
-    <section className="relative flex flex-col  items-center justify-end gap-12 h-screen max-w-full  mx-auto bg-gradient-to-b from-[#28044d] to-[#0E001C]">
+    <section className="relative flex flex-col  items-center justify-end gap-10 h-screen max-w-full  mx-auto bg-gradient-to-b from-[#28044d] to-[#0E001C]">
       <p className="px-5 text-lg font-bold text-center text-gray-700 md:text-2xl dark:text-white">
         Want to know more about me? Ask my personal assistant 🤖
       </p>
       <ChatBot initialMessage="Someone out there?" />
-      <footer className="flex justify-center py-4 font-bold text-gray-700 dark:text-white">
-        Built with 💗 &amp; ☕️ &amp; 🧉 &amp; AI
+      <footer className="flex flex-col items-center justify-center gap-3 py-4 font-bold text-gray-700 dark:text-white">
+        <p>Built with 💗 &amp; ☕️ &amp; 🧉 &amp; AI</p>
+        <p className="text-sm text-zinc-200 dark:text-zinc-500">
+          © 2023 Maxi Calderón. All rights reserved.
+        </p>
       </footer>
     </section>
   );
