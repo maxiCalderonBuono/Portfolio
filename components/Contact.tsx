068a6fe31d8907e6bfc7934fc20236cc3cdb1fa5
Maxi <calderonmaxi@outlook.com>
2023-03-21 20:17:15 +0100

    test12

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,13 +4,12 @@ import { ChatBot } from "./ChatBot";
 
 const Contact = () => {
   return (
-    <section className="relative z-0 flex flex-col items-center justify-center h-screen max-w-full gap-5 mx-auto bg-gradient-to-b from-[#28044d] to-[#0E001C] ">
-      <p className="px-5 py-4 mt-10 text-lg font-bold text-center text-gray-700 md:text-2xl dark:text-white">
+    <section className="relative flex flex-col  items-center justify-end gap-12 h-screen max-w-full  mx-auto bg-gradient-to-b from-[#28044d] to-[#0E001C]">
+      <p className="px-5 text-lg font-bold text-center text-gray-700 md:text-2xl dark:text-white">
         Want to know more about me? Ask my personal assistant 🤖
       </p>
       <ChatBot initialMessage="Someone out there?" />
-
-      <footer className="absolute bottom-0 flex justify-center py-4 font-bold text-gray-700 dark:text-white">
+      <footer className="flex justify-center py-4 font-bold text-gray-700 dark:text-white">
         Built with 💗 &amp; ☕️ &amp; 🧉 &amp; AI
       </footer>
     </section>
